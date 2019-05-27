3c6c3c822e4455508bf6682febd1a4abf15aea9e
zhongshuai <zhongshuai@megvii.com>
2019-05-27 22:22:56 +0800

    m

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+<!--
+ * @Description: 
+ * @Author: zhongshuai
+ * @Date: 2019-05-27 22:20:40
+ * @LastEditors: zhongshuai
+ * @LastEditTime: 2019-05-27 22:22:42
+ -->
 # express-web
+## express-web node 静态服务器
---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: zhongshuai
+ * @Date: 2019-05-27 22:12:14
+ * @LastEditors: zhongshuai
+ * @LastEditTime: 2019-05-27 22:19:02
+ */
+//express_demo.js 文件
+var express = require('express');
+var http = require('http'); 
+
+var app = express();
+app.use(express.static('./public'))
+
+const httpServer = http.createServer(app);
+httpServer.listen(5000, '0.0.0.0', () => {
+  console.log('success');
+});
