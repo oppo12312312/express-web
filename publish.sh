d41ed5ab8d410d44251cb34fadf54feb6beed8a8
zhongshuai <zhongshuai@megvii.com>
2019-05-27 22:46:36 +0800

    m

---
+++ publish.sh
@@ -0,0 +1,8 @@
+###
+ # @Description: 
+ # @Author: zhongshuai
+ # @Date: 2019-05-27 22:38:10
+ # @LastEditors: zhongshuai
+ # @LastEditTime: 2019-05-27 22:40:54
+###
+cp -r -f /Users/zhongshuai/Documents/HBuilderProjects/uni-sj/unpackage/dist/build/h5/* /Users/zhongshuai/Desktop/work/1-project/study/express-web/public/
